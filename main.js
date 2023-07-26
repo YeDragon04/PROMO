1ac24e0309842776b07cc2bfb047250d8f4d82b4
caseresdanvictor@gmail.com <caseresdanvictor@gmail.com>
2023-07-26 15:28:50 +0800

    Optimize fo Chrome and Fix the Overflow issue of the winner section.

--- main.js
+++ main.js
@@ -1,19 +1,112 @@
 const getStarted2 = document.getElementById("getStarted2");
-const websiteDescription = document.getElementById("websiteDescription");
-const aligner = document.getElementById("aligner");
+const websiteDescription2 = document.getElementById("websiteDescription2");
+const aligner2 = document.getElementById("aligner2");
 const footer = document.getElementById('footer');
+const numberList = document.getElementById("winningNumbers");
+const winnerList = document.getElementById("winners");
+const description = document.getElementById("description");
+const carouselSize = document.getElementById("carousel");
+const carouselBackground = document.getElementById("carouselBackground");
+const menu = document.getElementById("menu");
+const tagLine = document.getElementById("tagLine");
+const how = document.getElementById("how")
+const graphicDesign = document.getElementById("graphicDesign")
+const getStarted1 = document.getElementById("getStarted1");
+const websiteDescription1= document.getElementById("websiteDescription1");
+const aligner1 = document.getElementById("aligner1");
 
 
-const fontTimer = setInterval(sizeResponsive, 100);
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+const section4 = document.getElementById("section4");
+
+const fontTimer = setInterval(sizeResponsive, 10);
 let fontSize;
 
 function sizeResponsive() {
   let width = window.innerWidth;
+  let height = window.innerHeight;
+  /*allSections*/
+  console.log(width);
+  fontSize = width / 9;
+  fontSize = fontSize * 16;
+  fontSize = fontSize / 16;
+  fontSize = fontSize * 1.25;
+  section1.style.height = fontSize + "rem";
+  section2.style.height = fontSize + "rem";
+  section3.style.height = fontSize + "rem";
+  section4.style.height = fontSize + "rem";
+  /*section1*/
+  fontSize = width * .05;
+  fontSize = fontSize / 16;
+  tagLine.style.fontSize = fontSize + "rem";
+  /*header*/
+  fontSize = width * .02;
+  fontSize = fontSize / 16;
+  menu.style.fontSize = fontSize + "rem";
+  fontSize = width * .05;
+  fontSize = fontSize / 16;
+  menu.style.height = fontSize + "rem";
+  /*tagline*/
+  fontSize = width * .04;
+  fontSize = fontSize / 16;
+  tagLine.style.fontSize = fontSize + "rem";
+  /*carousel*/
+  fontSize = width * .87;
+  fontSize = fontSize / 16;
+  carouselSize.style.width = fontSize + "rem";
+  carouselBackground.style.height = carouselSize.clientHeight / 16 + "rem";
+  /*introduction*/
+  /*websiteDescription*/
+  fontSize = width * .03;
+  fontSize = fontSize / 16;
+  websiteDescription1.style.fontSize = fontSize + "rem";
+  /*getStarted*/
+  fontSize = width * .25;
+  fontSize = fontSize / 16;
+  getStarted1.style.width = fontSize + "rem";
+  fontSize = width * .03;
+  fontSize = fontSize / 16;
+  getStarted1.style.fontSize = fontSize + "rem";
+  fontSize = width * .0375;
+  fontSize = fontSize / 16;
+  getStarted1.style.borderRadius = fontSize + "rem";
+  /*aligner*/
+  fontSize = width * .04;
+  fontSize = fontSize / 16;
+  aligner1.style.fontSize = fontSize + "rem";
+  /*section2*/
+  /*how*/
+  fontSize = width * .075;
+  fontSize = fontSize / 16;
+  how.style.fontSize = fontSize + "rem";
+  /*graphicDesign*/
+  fontSize = width * .075;
+  fontSize = fontSize / 16;
+  graphicDesign.style.fontSize = fontSize + "rem";
+  fontSize = width * .7;
+  fontSize = fontSize / 16;
+  graphicDesign.style.width = fontSize + "rem";
+  /*section3*/
+  /*description*/
+  fontSize = width * .03;
+  fontSize = fontSize / 16;
+  description.style.fontSize = fontSize + "rem";
+  /*numberList*/
+  fontSize = width * .04;
+  fontSize = fontSize / 16;
+  numberList.style.fontSize = fontSize + "rem";
+  /*winnerList*/
+  fontSize = width * .04;
+  fontSize = fontSize / 16;
+  winnerList.style.fontSize = fontSize + "rem";
+  /*section4*/
   /*conclusion*/
   /*websiteDescription*/
   fontSize = width * .03;
   fontSize = fontSize / 16;
-  websiteDescription.style.fontSize = fontSize + "rem";
+  websiteDescription2.style.fontSize = fontSize + "rem";
   /*getStarted*/
   fontSize = width * .25;
   fontSize = fontSize / 16;
@@ -27,7 +120,7 @@ function sizeResponsive() {
   /*aligner*/
   fontSize = width * .04;
   fontSize = fontSize / 16;
-  aligner.style.fontSize = fontSize + "rem";
+  aligner2.style.fontSize = fontSize + "rem";
   /*footer*/
   fontSize = width * .02;
   fontSize = fontSize / 16;
@@ -308,9 +401,6 @@ function carousel() {
 
 let winners = ["Rad", "Dave", "Diana", "May"];
 let winnerNumbers = ["1", "2", "3", "4"];
-
-const numberList = document.getElementById("winningNumbers");
-const winnerList = document.getElementById("winners");
 
 for (i = 0; i < winners.length; ++i) {
   var li = document.createElement("li");
